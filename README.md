c3c028546aab13cc71aad508e93a4a58fe98e90d
Will Fisher <wfisher@chef.io>
2018-09-04 09:44:51 -0700

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chefserver
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: chefserver
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
