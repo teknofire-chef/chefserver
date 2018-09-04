bc945e893ab6d4399d4bdb0c8f4601abef077421
Will Fisher <wfisher@chef.io>
2018-09-04 12:40:02 -0700

    bump version to 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures chefserver'
 long_description 'Installs/Configures chefserver'
-version '0.1.1'
+version '0.1.2'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
